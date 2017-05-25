937e789c7d908ce18c6722bf34bc1a830393bc8a
Frants Stentzer <frants2205@gmail.com>
2017-05-25 10:44:17 +0200

    Hey

--- src/app/frivillig/frivillig-list/frivillig-list.component.ts
+++ src/app/frivillig/frivillig-list/frivillig-list.component.ts
@@ -8,13 +8,12 @@ import { Frivillig } from "app/frivillig/frivillig.model";
 })
 export class FrivilligListComponent implements OnInit {
   frivillige: Frivillig[] = [
-<<<<<<< HEAD
+
     new Frivillig(1, "Hans", "Hansen","Horsensvej", "hh@hotmil.com","12345678"),
-    new Frivillig(2, "Jens", "Jensen","Vejlevej","jj@hotmail.com","87654321")
-=======
+    new Frivillig(2, "Jens", "Jensen","Vejlevej","jj@hotmail.com","87654321"),
+
     new Frivillig(1, "Hans", "Hansen", "Hansvej 1", "hans@hansen.dk", "23445565"),
-    new Frivillig(2, "Jens", "Jensen", "Jensvej 2", "jens@jensen.dk", "12345789"),
->>>>>>> b729d581ef0adad7d32a68bf928ffd873160741a
+    new Frivillig(2, "Jens", "Jensen", "Jensvej 2", "jens@jensen.dk", "12345789")
   ];
   constructor() { }
 
