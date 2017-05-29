7a391376159d94ebfd2ffad4b19f4893ad0f5a05
Frants Stentzer <frants2205@gmail.com>
2017-05-29 11:06:43 +0200

    Casper er en skøge

--- src/app/frivillig/frivillig-list/frivillig-list.component.ts
+++ src/app/frivillig/frivillig-list/frivillig-list.component.ts
@@ -11,21 +11,13 @@ export class FrivilligListComponent implements OnInit {
 
     new Frivillig(1, "Hans", "Hansen","Horsensvej", "hh@hotmil.com","12345678"),
     new Frivillig(2, "Jens", "Jensen","Vejlevej","jj@hotmail.com","87654321"),
-<<<<<<< HEAD
-=======
 
->>>>>>> 1e8cc3e5b9e68c902b0caad99c3bd3f2d06e1e08
     new Frivillig(1, "Hans", "Hansen", "Hansvej 1", "hans@hansen.dk", "23445565"),
-<<<<<<< HEAD
-    new Frivillig(2, "Jens", "Jensen", "Jensvej 2", "jens@jensen.dk", "12345789"),
-<<<<<<< HEAD
 
-=======
-=======
+    new Frivillig(2, "Jens", "Jensen", "Jensvej 2", "jens@jensen.dk", "12345789"),
+
     new Frivillig(2, "Jens", "Jensen", "Jensvej 2", "jens@jensen.dk", "12345789")
->>>>>>> 937e789c7d908ce18c6722bf34bc1a830393bc8a
->>>>>>> 1e8cc3e5b9e68c902b0caad99c3bd3f2d06e1e08
-  ];
+  ]
   constructor() { }
 
   ngOnInit() {
